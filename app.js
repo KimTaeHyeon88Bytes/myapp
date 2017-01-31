dbab0a929614bac3d4c3b0bb361e22dfdeea126d
KimTaeHyeon <ththkim0406@naver.com>
2017-01-31 19:23:42 +0900

    MyFirstApp9

--- app.js
+++ app.js
@@ -14,4 +14,32 @@ app.listen(port, function() {
   console.log("server on!");
 });
 
-app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+//app.use(express.static(__dirname + "/public"));
+
+var data = {
+  count : 0
+};
+
+app.get("/", function(req, res) {
+  data.count++;
+  //console.log(data.count);
+  res.render("MyFirstEjs.ejs", data);
+});
+
+app.get("/reset", function(req, res){
+  data.count = 0;
+  res.render("MyFirstEjs.ejs", data);
+});
+
+app.get("/set/count", function(req, res){
+  if(req.query.count) {
+    data.count = req.query.count;
+  }
+  res.render("MyFirstEjs.ejs", data);
+});
+
+app.get("/set/:num", function(req, res) {
+  data.count = req.params.num;
+  res.render("MyFirstEjs.ejs", data);
+});
