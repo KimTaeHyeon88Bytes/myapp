d912473a178b831b1a11fefe939f5f9d0f79f849
KimTaeHyeon <ththkim0406@naver.com>
2017-01-31 15:44:41 +0900

    MyFirstApp2

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express");
+
+var app = express();
+
+/*app.get("/", function(req, res) {
+  res.send("Hello World!");
+});*/
+
+app.listen(8000, function() {
+  console.log("server on!");
+});
+
+app.use(express.static(__dirname + "/public"));
